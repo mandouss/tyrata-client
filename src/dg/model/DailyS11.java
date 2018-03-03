92d082e8b0e841a19b41e020c6a43f0464e8ce96
Yifan Li <yl506@duke.edu>
2018-03-03 13:30:39 -0500

    generator prototype ver 1.0

--- src/dg/model/DailyS11.java
+++ src/dg/model/DailyS11.java
@@ -1,7 +1,35 @@
 package dg.model;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import javafx.util.Pair;
 
 
 public class DailyS11 {
-
+	private LocalDate timestamp;
+	private int mileage;
+	private ArrayList<Pair<String, Double> > tireInfoAndS11_list; 
+	
+	public DailyS11(LocalDate timestamp, int mileage) {
+		super();
+		this.timestamp = timestamp;
+		this.mileage = mileage;
+	}
+	
+	public void addTireS11(String tireinfo, double s11) {
+		tireInfoAndS11_list.add(new Pair<String, Double>(tireinfo, s11));
+	}
+	
+	public void print() {
+		DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
+		System.out.println("Timestamp:" + timestamp.format(formatter));
+		System.out.println("Mileage: " + mileage);
+		
+		for (int i = 0; i < tireInfoAndS11_list.size(); i++) {
+			System.out.println("String: "+ tireInfoAndS11_list.get(i).getKey());
+			System.out.println("S11: " + tireInfoAndS11_list.get(i).getValue());
+		}
+	}
+	
 }
---
+++ src/dg/model/DataGenerator.java
@@ -0,0 +1,55 @@
+package dg.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+
+
+public class DataGenerator {
+
+	private IntegerProperty timeLength;
+	private IntegerProperty dailyMileage;
+	private int currentMileage;
+	private List<Tire> tireInfoList;
+	private LocalDate currentDate;
+	
+
+	public DataGenerator(ObjectProperty<LocalDate> startDate, IntegerProperty timeLength, IntegerProperty dailyMileage,
+			List<Tire> tireInfoList) {
+		super();
+		this.timeLength = timeLength;
+		this.dailyMileage = dailyMileage;
+		this.tireInfoList = tireInfoList;
+		this.currentDate = startDate.get();
+		this.currentMileage = 0;
+	}
+
+
+	private DailyS11 computeNextS11() {
+		
+		DailyS11 result = new DailyS11(currentDate, currentMileage);
+		for (int i = 0; i < tireInfoList.size(); i++) {
+			Random randomno = new Random();
+			double x = randomno.nextGaussian() * 0.1 + 1;
+			double s11_m = tireInfoList.get(i).getInitS11()  + this.currentMileage * 0.08 / 5000 * x;
+			String tireinfo = tireInfoList.get(i).getTireID() + " " + tireInfoList.get(i).getTirePos();
+			result.addTireS11(tireinfo, s11_m);
+		}
+		
+		currentDate.plusDays(1);
+		currentMileage += dailyMileage.get();
+		return result;
+	}
+	
+	public ArrayList<DailyS11> GenerateSeries() {
+		ArrayList<DailyS11> result = new ArrayList<>();
+		for (int i = 0; i < timeLength.get(); i++) {
+			result.add(computeNextS11());
+		}
+		return result;
+	}
+	
+}
