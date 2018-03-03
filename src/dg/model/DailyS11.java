c3d526c40b463fb8db9c8a60de241cd0b50f8545
haohongzhao <hz147@duke.edu>
2018-03-03 14:29:04 -0500

    delete main.java(for testing), add main entry(for testing) in DataGenerator.java

--- src/dg/model/DailyS11.java
+++ src/dg/model/DailyS11.java
@@ -3,6 +3,12 @@ package dg.model;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.util.Pair;
 
 
@@ -21,7 +27,6 @@ public class DailyS11 {
 	public void addTireS11(String tireinfo, double s11) {
 		tireInfoAndS11_list.add(new Pair<String, Double>(tireinfo, s11));
 	}
-	//
 	
 	public void print() {
 		DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
@@ -33,5 +38,24 @@ public class DailyS11 {
 			System.out.println("S11: " + tireInfoAndS11_list.get(i).getValue());
 		}
 	}
-	
+    public static void main(String[] args) {
+        //tireInfo_list
+        Tire tireLF = new Tire("C-1", "LF", -2.0);
+        Tire tireRF = new Tire("C-154", "RF", -1.5);
+        List<Tire> tireList = new ArrayList<>();
+        tireList.add(tireLF);
+        tireList.add(tireRF);
+        //startDate
+        ObjectProperty<LocalDate> startDate = new SimpleObjectProperty<>(LocalDate.of(2018,03,01));
+        //timeLength
+        IntegerProperty timeLength = new SimpleIntegerProperty(10);
+        //dailyMileage
+        IntegerProperty dailyMileage = new SimpleIntegerProperty(15);
+        //dataGen
+        DataGenerator dataGen = new DataGenerator(startDate, timeLength, dailyMileage, tireList);
+        //day_list
+        ArrayList<DailyS11> result = dataGen.GenerateSeries();
+        result.forEach((dailyResult) -> dailyResult.print());
+        //
+    }
 }
--- src/dg/model/DataGenerator.java
+++ src/dg/model/DataGenerator.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Random;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
 
 public class DataGenerator {
@@ -26,8 +28,6 @@ public class DataGenerator {
 		this.currentDate = startDate.get();
 		this.currentMileage = 0;
 	}
-
-	//this is a try
 
 	private DailyS11 computeNextS11() {
 		
@@ -52,5 +52,26 @@ public class DataGenerator {
 		}
 		return result;
 	}
+	
+    public static void main(String[] args) {
+        //tireInfo_list
+        Tire tireLF = new Tire("C-1", "LF", -2.0);
+        Tire tireRF = new Tire("C-154", "RF", -1.5);
+        List<Tire> tireList = new ArrayList<>();
+        tireList.add(tireLF);
+        tireList.add(tireRF);
+        //startDate
+        ObjectProperty<LocalDate> startDate = new SimpleObjectProperty<>(LocalDate.of(2018,03,01));
+        //timeLength
+        IntegerProperty timeLength = new SimpleIntegerProperty(10);
+        //dailyMileage
+        IntegerProperty dailyMileage = new SimpleIntegerProperty(15);
+        //dataGen
+        DataGenerator dataGen = new DataGenerator(startDate, timeLength, dailyMileage, tireList);
+        //day_list
+        ArrayList<DailyS11> result = dataGen.GenerateSeries();
+        result.forEach((dailyResult) -> dailyResult.print());
+        //
+    }
 	
 }
