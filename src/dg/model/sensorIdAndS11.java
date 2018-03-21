8c29759e33ae99f2c2293211256f62377ceb0436
zz134 <zz134@duke.edu>
2018-03-21 11:56:13 -0400

    add a class named sendorIdAndS11 to replace pIAR<String Double>

---
+++ src/dg/model/sensorIdAndS11.java
@@ -0,0 +1,34 @@
+package dg.model;
+
+
+public class sensorIdAndS11 {
+	private String sensorID;
+	private Double S11;
+	
+	public sensorIdAndS11() {
+		super();
+		this.sensorID = null;
+		this.S11 = (double) 0;
+	}
+	public sensorIdAndS11(String sensorId, Double s11) {
+		super();
+		this.sensorID = sensorId;
+		this.S11 = s11;
+	}
+	public void setSensorID(String sensorId) {
+		this.sensorID = sensorId;
+	}
+	
+	public String getSensorID() {
+		return sensorID;
+	}
+	
+	public void setS11(Double s11) {
+		this.S11 = s11;
+	}
+	
+	public Double getS11() {
+		return S11;
+	}
+	
+}
