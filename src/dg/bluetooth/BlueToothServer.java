841de3b0b5ab1f5808c7711f9b744d199d8a814b
yz395 <yz395@duke.edu>
2018-04-08 16:14:40 -0400

    add close connection protocol implementation

--- src/dg/bluetooth/BlueToothServer.java
+++ src/dg/bluetooth/BlueToothServer.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import javax.bluetooth.DiscoveryAgent;
 import javax.bluetooth.LocalDevice;
 import javax.bluetooth.RemoteDevice;
@@ -23,25 +25,7 @@ public class BlueToothServer extends Task {
  private StreamConnectionNotifier notifier;
  //private byte[] acceptdByteArray;
  
- public BlueToothServer() {
-	 /*try {
-		 local = LocalDevice.getLocalDevice();
-		 if(!local.setDiscoverable(DiscoveryAgent.GIAC)) {
-			 System.out.println("Please set your local bluetooth as discoverable");
-		 }
-		 
-	 //btspp stands for RFCOMM connection
-		 //System.out.println(new UUID("fa87c0d0afac11de8a390800200c9a66",false).toString());
-		 String url = "btspp://localhost:" + new UUID("fa87c0d0afac11de8a390800200c9a66",false).toString() + ";authenticate=false;encrypt=false;name=RemoteBluetooth";
-	 //open the service
-		 System.out.println("I am stuck here!");
-		 this.notifier = (StreamConnectionNotifier)Connector.open(url);
-		 System.out.println("I pass here!");
-	 }	                 
-	 catch(IOException e) {
-		 e.printStackTrace();
-	 }*/
- }
+ public BlueToothServer() {}
  public void run() { 
 	 try {
 		 local = LocalDevice.getLocalDevice();
@@ -58,33 +42,66 @@ public class BlueToothServer extends Task {
 		 System.out.println("I pass here!");
 		 System.out.println("Make Service visible to remote client!");
   		 streamConnection = (this.notifier).acceptAndOpen();
+  		 
   		 DataOutputStream optStream = streamConnection.openDataOutputStream();
+  		 DataInputStream inStream = streamConnection.openDataInputStream();
   		 BufferedReader bufferedReader = new BufferedReader(new FileReader("F:\\test2.xml"));
   		 StringBuffer stringBuffer = new StringBuffer();
   		 String line = null;
   		 while(((line = bufferedReader.readLine())!=null)) {
   			 stringBuffer.append(line).append("\n");
   		 }
-  		 //StringBuffer stringbuffer = new StringBuffer();
-  		// for (int i = 0; i < 100; ++i) {
-  		//	 stringbuffer.append(" Hello! This is a test for the communication between Tyrata Android Mobile and Tyrata Client");
-  		 //}
-  		 //String test = new String(" Hello! This is a test for the communication between Tyrata Android Mobile and Tyrata Client");
-  		 //System.out.println(stringBuffer);
-  		 System.out.println("Writing file to the other device....");
-  		 optStream.write(stringBuffer.toString().getBytes());
-  		 System.out.println("Writing completed....");
   		 bufferedReader.close();
-  	}
+  		 System.out.println("Writing file to the other device....");
+  		 //optStream.write(stringBuffer.toString().getBytes());
+  		 boolean has_succeeded = false;
+  		 /*
+  		  * The following while loop is to make sure that the data transmitted is not corrupted on its way to Android Mobile. If that is true
+  		  * the server side will retransmit the data 
+  		  */
+  		 while (!has_succeeded) {
+  			 byte[] send_byte_array = stringBuffer.toString().getBytes();
+  			 int off = 0;
+  			 while(off < send_byte_array.length) {
+  				 int len = 512;
+  				 if (off+len > send_byte_array.length) len = send_byte_array.length - off;
+  				 optStream.write(send_byte_array, off, len);
+  				 off += len;
+  				 TimeUnit.MILLISECONDS.sleep(50);
+  			 }
+  			 byte EOF = 0;
+  			 optStream.write(EOF);
+  			 System.out.println("Writing completed....");	 
+  			 int timer = 0;
+  			 while(timer < 10000) { //polling will end if the time elapses 10000 miliseconds
+  				 byte [] read_byte_array = new byte [1];
+  				 if(inStream.available() > 0) {
+  					 inStream.read(read_byte_array, 0, 1);
+  					 String check = new String(read_byte_array);
+  					 if (check.contentEquals("T")) {
+  	  					has_succeeded = true; //If I received T, the client side received all the data correctly. I will close connection
+  					 }
+  					 break; //I will break anyway, because it will either succeed or fail
+  				 }
+  				 //if not available, I still don't know the result. So I sleep for several miliseconds and poll again
+  				 TimeUnit.MILLISECONDS.sleep(500); 
+  				 timer += 500;
+  			 }
+  		 }	
+	 }
 	 catch (IOException  e) {
+		 e.printStackTrace();
+	 } 
+	 catch (InterruptedException e) {
+		// TODO Auto-generated catch block
 			e.printStackTrace();
-	} 
+	 } 
  }
-@Override
-protected Object call() throws Exception {
+ @Override
+	protected Object call() throws Exception {
 	// TODO Auto-generated method stub
-	run();
-	return null;
+		run();
+		return null;
+	}	
 }
  
-}
