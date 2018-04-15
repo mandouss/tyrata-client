f6fe981c43f3b3428cd4a02ac7da06ba396fe623
zz134 <zz134@duke.edu>
2018-04-15 12:53:22 -0400

    adjust tire sensor id format

--- src/dg/MainApp.java
+++ src/dg/MainApp.java
@@ -43,10 +43,10 @@ public class MainApp extends Application {
 	private ArrayList<DailyS11> s11List = new ArrayList<DailyS11>();
 
 	public MainApp() {
-		tireData	.add(new Tire("T-1234","LF",-1.3332,3.5));
-		tireData	.add(new Tire("T-1302","RF",-2.4309,3.5));
-		tireData	.add(new Tire("T-4124","LR",-1.0223,3.5));
-		tireData	.add(new Tire("T-9175","RR",-2.222,3.5));
+		tireData	.add(new Tire("T-0001","LF",-1.3332,35));
+		tireData	.add(new Tire("T-0002","RF",-2.4309,35));
+		tireData	.add(new Tire("T-0003","LR",-1.0223,35));
+		tireData	.add(new Tire("T-0004","RR",-2.222,35));
 	}
 
 	/**
